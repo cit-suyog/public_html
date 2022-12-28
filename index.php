ede2e2b80640db50fba424b62e7d4e92b66b40be
cit.suyog <cit.suyogbhavsar2019@gmail.com>
2022-12-28 19:04:03 +0530

    Main

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
 echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
-echo "Hello Suyog Welcome 1.";
 ?>
