2d38e083ce0073fc68a0a9cdc883787ab50ff0e3
cit.suyog <cit.suyogbhavsar2019@gmail.com>
2022-12-29 14:19:43 +0530

    S

--- index.php
+++ index.php
@@ -7,8 +7,4 @@ echo "Hello Suyog Welcome 3.";
 echo "<br>";
 echo "Hello Suyog Welcome 4.";
 echo "<br>";
-echo "Hello Suyog Welcome 5.";
-echo "<br>";
-echo "Hello Suyog Welcome 6.";
-echo "<br>";
 ?>
