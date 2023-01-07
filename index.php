8464a6697021c62b8577b006cb92338910ea2478
cit.suyog <cit.suyogbhavsar2019@gmail.com>
2023-01-07 13:05:46 +0530

    Shoaib

--- index.php
+++ index.php
@@ -2,9 +2,5 @@
 for ($i = 0; $i < 10; $i++) {
     echo "Hello Suyog Welcome $i.";
     echo "<br>";
-    echo "Hello Suyog Welcome $i.";
-    echo "<br>";
-    echo "Hello Suyog Welcome $i.";
-    echo "<br>";
 }
 ?>
